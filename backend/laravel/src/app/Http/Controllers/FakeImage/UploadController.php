a506b7d0c909a5a86d4df06e46908ee4524cfc47
Oleksandr Nykytin <keeper@ninydev.com>
2024-01-19 20:41:32 +0200

    Upload Images

---
+++ backend/laravel/src/app/Http/Controllers/FakeImage/UploadController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\FakeImage;
+
+use App\Http\Controllers\Controller;
+use App\Models\FakeImageModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class UploadController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    function upload(Request $request)
+    {
+        // Проверяем, что прислал пользователь
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'photo' => 'required|file|mimes:jpeg,png,jpg,gif,webp|max:8096',
+            'back' => 'required|file|mimes:jpeg,png,jpg,gif,webp|max:8096',
+        ]);
+
+        $fakePhoto = new FakeImageModel();
+        $fakePhoto->name = $request->input('name');
+        $fakePhoto->author_id = $request->user()->id;
+        $fakePhoto->original_photo_url = "";
+        $fakePhoto->original_back_url = "";
+        $fakePhoto->save();
+
+        // Получаем файл из запроса
+        $filePhoto = $request->file('photo');
+        $filePhotoName = time() . '_' . $filePhoto->getClientOriginalName();
+        // Получите путь к файлу
+        $filePhotoPath = $filePhoto->storeAs('public/fake_photos/'
+            . $request->user()->id . '/' . $fakePhoto->id, $filePhotoName);
+        // Получите URL файла
+        $filePhotoUrl = url(Storage::url($filePhotoPath));
+
+        // Получаем файл из запроса
+        $fileBack = $request->file('back');
+        $fileBackName = time() . '_' . $fileBack->getClientOriginalName();
+        // Получите путь к файлу
+        $fileBackPath = $filePhoto->storeAs('public/fake_photos/'
+            . $request->user()->id . '/'. $fakePhoto->id, $fileBackName);
+        // Получите URL файла
+        $fileBackUrl = url(Storage::url($fileBackPath));
+
+
+        $fakePhoto->original_photo_url = $filePhotoUrl;
+        $fakePhoto->original_back_url = $fileBackUrl;
+
+        $fakePhoto->save();
+
+        return response()->json($fakePhoto);
+    }
+}
--- backend/laravel/src/routes/api.php
+++ backend/laravel/src/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\FakeImage\UploadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +24,5 @@ Route::post('register',[AuthController::class,'register']);
 Route::post('login', [AuthController::class,'login']);
 Route::post('refresh', [AuthController::class,'refresh']);
 Route::post('logout', [AuthController::class,'logout']);
+
+Route::post('fake_images/upload', [UploadController::class,'upload']);
