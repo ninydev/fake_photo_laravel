6af14c1221c448c760c1d62fda522a46bcc6a03e
Oleksandr Nykytin <keeper@ninydev.com>
2024-01-19 20:19:50 +0200

    Project Auth

--- backend/laravel/src/app/Models/User.php
+++ backend/laravel/src/app/Models/User.php
@@ -8,8 +8,9 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -47,5 +48,18 @@ class User extends Authenticatable
     public function fakeImages(): HasMany
     {
         return $this->hasMany(FakeImageModel::class, 'author_id', 'id');
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [
+            'email'=>$this->email,
+            'name'=>$this->name
+        ];
     }
 }
